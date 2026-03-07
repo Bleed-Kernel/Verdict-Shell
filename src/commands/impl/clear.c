dcb40f2c70c86f3bace30d7e9da6c8fb01371576
Myles "Mellurboo" Wilson <102391892+Mellurboo@users.noreply.github.com>
2026-03-07 23:59:07 +0000

    Revert "really really shitty clear"
    This reverts commit c2f3bcbccaf9d708e4943852671e18b6dafe611a.

--- src/commands/impl/clear.c
+++ src/commands/impl/clear.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 #include <commands/commands.h>
 #include <main.h>
-#include <devices/console.h>
-#include <syscalls/ioctl.h>
 
 int cmd_clear(shell_cmd_t *cmd) {
     (void)cmd;
-    tty_cursor_t home = { .x = 0, .y = 0 };
-    (void)_ioctl(1, TTY_IOCTL_SET_CURSOR, &home);
-    printf("\x1b[2J");
+    printf("\x1b[J");
     return 0;
 }
--- src/commands/impl/theme.c
+++ src/commands/impl/theme.c
@@ -2,8 +2,6 @@
 
 #include <commands/commands.h>
 #include <theme.h>
-#include <devices/console.h>
-#include <syscalls/ioctl.h>
 
 int cmd_theme(shell_cmd_t *cmd) {
     if (cmd->argc < 2) {
@@ -18,9 +16,7 @@ int cmd_theme(shell_cmd_t *cmd) {
         return -1;
     }
 
-    tty_cursor_t home = { .x = 0, .y = 0 };
-    (void)_ioctl(1, TTY_IOCTL_SET_CURSOR, &home);
-    printf("%s\x1b[2J", theme_background_bg());
+    printf("%s\x1b[J", theme_background_bg());
     printf("theme loaded: %s\n", theme_active_path());
     return 0;
 }
