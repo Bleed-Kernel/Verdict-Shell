e9a48758a180e46ec56be651f3e29e045cb857ab
mellurboo <mellurblur@gmail.com>
2026-05-24 03:39:00 +0100

    changed power options

--- src/commands/impl/reboot.c
+++ src/commands/impl/reboot.c
@@ -1,8 +1,14 @@
-#include <syscalls/reboot.h>
+#include <stdio.h>
+#include <syscalls/open.h>
+#include <syscalls/ioctl.h>
+#include <syscalls/close.h>
 #include <commands/commands.h>
+#include <fcntl.h>
 
 int cmd_reboot(shell_cmd_t *cmd){
     (void)cmd;
-    _reboot();
+    int pwr = _open("/dev/power", O_RDWR);
+    _ioctl(pwr, 0xEEEEEEEE, NULL);
+    _close(pwr);
     return 0;
 }
--- src/commands/impl/shutdown.c
+++ src/commands/impl/shutdown.c
@@ -1,8 +1,14 @@
-#include <syscalls/shutdown.h>
+#include <stdio.h>
+#include <syscalls/open.h>
+#include <syscalls/ioctl.h>
+#include <syscalls/close.h>
 #include <commands/commands.h>
+#include <fcntl.h>
 
 int cmd_shutdown(shell_cmd_t *cmd){
     (void)cmd;
-    _shutdown();
+    int pwr = _open("/dev/power", O_RDWR);
+    _ioctl(pwr, 0xFFFFFFFF, NULL);
+    _close(pwr);
     return 0;
 }
